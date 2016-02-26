e32ea80ac2c420963b8e274485020d29f5ef13de
acDante <shana0123@163.com>
2016-02-26 21:35:09 +0800

    Revert "Revert "乘客线程类""
    This reverts commit 986c42a1e77c4d1018e1ff7f0ca27deaf907d318.

--- PeopleThread.cpp
+++ PeopleThread.cpp
@@ -18,19 +18,18 @@ void PeopleThread::run()
               people.sendSignal(v_time);//乘客发信号
               wait();//进行等待，乘客线程被挂起，等待电梯线程的唤醒
           }
-          current = v_time->GetCurVTime()
-          if(- current.secsTo(people.requestTime) <= people.tolerationTime )
+          current = v_time->GetCurVTime();
+          if(- current.secsTo(people.requestTime)<= people.tolerationTime)
           {
               people.getIn();
               wait();
               people.getOut();
-              people.arrivaltime = gettime();
-              people.staytime = randomNum();//生成随机等待时间
           }
           else
           {
-              people.arrivaltime = gettime();
-              people.staytime = randomNum();
+              people.arrivalTime = v_time->GetCurVTime();
+              qsrand(QTime(0, 0, 0).secsTo(QTime::currentTime()));
+              people.stayTime = MIN_STAY_TIME + qrand() % (MAX_STAY_TIME - MIN_STAY_TIME + 1); // 重新生成乘客在该楼层的停留时间
           }
     }
 }
